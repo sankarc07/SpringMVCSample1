3dfa1ee97e9b804c56b2803d6a4be454bd65e1f4
c-chelsa <SChellamuthu.consultant@consumer.org>
2018-10-31 17:58:28 +0530

    Update ResourceBundle to ReloadableResourceBundle

--- src/com/spring/config/AppConfig.java
+++ src/com/spring/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -18,11 +19,11 @@ public class AppConfig extends WebMvcConfigurerAdapter {
 		return resolver;
 	}
 
-//	@Bean(name = "messageSource")
-//	public ResourceBundleMessageSource bundleMessageSource() {
-//		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-//		messageSource.setBasenames(new String[] {"/WEB-INF/validation"});
-//		return messageSource;
-//	}
+	@Bean(name = "messageSource")
+	public ReloadableResourceBundleMessageSource bundleMessageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasenames(new String[] { "/WEB-INF/resources/validation" });
+		return messageSource;
+	}
 
 }
