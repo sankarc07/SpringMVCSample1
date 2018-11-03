d6c0b26186185bbc946a90dba3934e001a789029
Sankar C <36528646+sankarc07@users.noreply.github.com>
2018-11-03 17:23:12 +0530

    update controller

--- src/com/spring/controller/SampleController.java
+++ src/com/spring/controller/SampleController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
@@ -24,6 +25,8 @@ import com.spring.validator.DineInFormValidator;
 @Controller
 public class SampleController {
 
+	private static final Logger logger = Logger.getLogger(SampleController.class);
+
 	@Autowired
 	DineInFormValidator dineInFormValidator;
 
@@ -38,12 +41,12 @@ public class SampleController {
 		return "index";
 	}
 
-	@InitBinder
-	public void initBinder(WebDataBinder binder) {
-		SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
-		dateFormat.setLenient(true);
-		binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
-	}
+//	@InitBinder
+//	public void initBinder(WebDataBinder binder) {
+//		SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+//		dateFormat.setLenient(true);
+//		binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+//	}
 
 	private List<String> restaurantList() {
 		List<String> restaurantList = new ArrayList<String>();
@@ -65,14 +68,24 @@ public class SampleController {
 
 	@RequestMapping(value = "/dineInPage", method = RequestMethod.GET)
 	public String dineInPage(Model model) {
+		//logs debug message
+		if(logger.isDebugEnabled()){
+			logger.debug("dineInPage is executed!");
+		}
 		model.addAttribute("dineInForm", new DineInForm());
 		model.addAttribute("restaurantList", restaurantList());
 		model.addAttribute("timeList", timeList());
+		//logs exception
+		logger.error("This is Error message", new Exception("Testing"));
 		return "dineInForm";
 	}
 
 	@RequestMapping(value = "/reserve", method = RequestMethod.POST)
 	public String reserve(Model model, @ModelAttribute DineInForm dineInForm, BindingResult error) {
+		//logs debug message
+		if(logger.isDebugEnabled()){
+			logger.debug("reserve is executed!");
+		}
 		dineInFormValidator.validate(dineInForm, error);
 		if (error.hasErrors()) {
 			model.addAttribute("restaurantList", restaurantList());
@@ -81,6 +94,8 @@ public class SampleController {
 		}
 		model.addAttribute("dineInForm", dineInForm);
 		model.addAttribute("fileUploader", new FileUploader());
+		//logs exception
+		logger.error("This is Error message", new Exception("Testing"));
 		return "dineInSuccess";
 	}
 }
